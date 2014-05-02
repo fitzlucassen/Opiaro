bb28315e36b12cd8d9361039d3063f7216df461a
Thibault <thibault.dulon@gmail.com>
2014-05-02 13:41:57 +0200

    no message

--- Website/MVC/View/Home/index.php
+++ Website/MVC/View/Home/index.php
@@ -6,7 +6,7 @@
 	var url = '<?php echo __image_directory__ ; ?>';
 </script>
 <script type="text/javascript" src="/<?php echo __js_directory__  ; ?>/Base/base64Manager.js"></script>
-<script type="text/javascript" src="/<?php echo __js_directory__  ; ?>/interfaceManagerView.js"></script>
+<script type="text/javascript" src="/<?php echo __js_directory__  ; ?>/View/interfaceManagerView.js"></script>
 <script type="text/javascript" src="/<?php echo __js_directory__  ; ?>/baseInterface.js"></script>
 <?php
     // inclure ci-dessus les balises à inclure dans la balise <head> du layout
